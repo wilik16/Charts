b28f07f8d9748be73e1855274e303b163242b6aa
wilik <wilik@bukalapak.com>
2022-07-21 14:38:18 +0700

    change swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -26,6 +26,5 @@ let package = Package(
             name: "Charts",
             dependencies: [.product(name: "Algorithms", package: "swift-algorithms")]
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
